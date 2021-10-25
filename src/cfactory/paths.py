13eea90d620bfa1d1e138e268a7512c8018d1755
Gabe Ingram <gabriel.ingram@cognitivespace.com>
2021-10-25 01:00:41 -0600

    Add paths file

---
+++ src/cfactory/paths.py
@@ -0,0 +1,21 @@
+import os
+
+cfactory_top = os.path.dirname(
+        os.path.abspath(__file__)
+        )
+collections_path = os.path.join(
+        cfactory_top,
+        "collections"
+        )
+config_path = os.path.join(
+        cfactory_top,
+        "__config__"
+        )
+std_factories_path = os.path.join(
+        cfactory_top,
+        "std_factories"
+        )
+utils_path = os.path.join(
+        cfactory_top,
+        "utils"
+        )
