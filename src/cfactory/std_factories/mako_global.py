4ea345024c8a163c775f0aaee26818453feb7c44
Gabe Ingram <gabriel.ingram@cognitivespace.com>
2021-10-25 23:07:35 -0600

    Remove std_factories

--- src/cfactory/std_factories/mako_global.py
+++
@@ -1,9 +0,0 @@
-import illuminate.__config__.illuminate_config as il_cfg
-from mako.lookup import TemplateLookup
-
-class_ids = {}
-trampoline_classes = {}
-trampoline_bases = {}
-class_conversions = {}
-
-dash_eye = []
