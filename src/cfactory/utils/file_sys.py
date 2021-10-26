a54bcd5ebc320e56bbab640a5222545210cbc62d
Gabe Ingram <gabriel.ingram@cognitivespace.com>
2021-10-25 23:08:01 -0600

    File sys utils update

--- src/cfactory/utils/file_sys.py
+++ src/cfactory/utils/file_sys.py
@@ -21,26 +21,8 @@ class cd(object):
             traceback.print_exception(exc_type, exc_value, tb)
         return
 
-
-def get_files_by_ext(ext: str, dir_in: str=os.getcwd(), include_hidden=False) -> typing.List[str]:
-
-    with cd(dir_in):
-        if not include_hidden:
-            out = [ext_file for ext_file in os.listdir() if ext_file.endswith(ext) and not ext_file.startswith(".")]
-        else:
-            out = [ext_file for ext_file in os.listdir() if ext_file.endswith(ext)]
-
-    return out
-
-def in_pkg_dir(method):
-
-    @functools.wraps(method)
-    def _in_pkg_dir(object_in: 'IlluminatePackage', *args, **kwargs):
-        out = None
-        with cd(object_in.illuminate_pkg_abspath):
-            out = method(object_in, *args, **kwargs)
-
-        return out
-
-    return _in_pkg_dir
-
+def path_from_fs_root(path: str, start_from: str = os.getcwd()) -> str:
+    return (
+            path if path.startswith(os.sep) else
+            os.path.join(start_dir, path)
+            )
