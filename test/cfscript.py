13de21f3c46599129fd084b24c4852e2ab64ca78
Gabriel Ingram <gabriel.ingram@colorado.edu>
2021-11-07 10:14:01 -0700

    Update test script with cybind assembler

--- test/cfscript.py
+++ test/cfscript.py
@@ -1,9 +1,10 @@
 import cfactory.factory as factory
+from cfactory.assemblers.cybind.assembler import CybindAssembler
 import os
-
 
 factory.project_name = "cfactory_test"
 factory.cache_dir = "cfactory_test_cache"
 factory.ccm_state_dir = os.path.join(factory.cache_dir, "ccm_test")
 
-
+cybind = CybindAssembler("test")
+cybind.add_files("test_hh/*.hh")
