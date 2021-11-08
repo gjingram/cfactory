37187e3e22b4e106996dc207e0bc7f45ea7570b5
Gabe Ingram <gabriel.ingram@cognitivespace.com>
2021-11-07 20:53:44 -0700

    Simplify 'map_headers,' call it automatically when registering a cybind module

--- src/cfactory/assemblers/cybind/cybind.py
+++ src/cfactory/assemblers/cybind/cybind.py
@@ -14,10 +14,9 @@ def register_module(cybind_module: "CybindModule") -> bool:
                 )
         sys.exit(-1)
     module_registry[cybind_module.module_name] = cybind_module
+    map_headers(cybind_module)
     return
 
-def map_headers() -> None:
-    for cybind_module in module_registry.values():
-        for module_header in cybind_module.module_headers:
-            header_map[module_header] = cybind_module.module_name
+def map_headers(cybind_module: "CybindModule") -> None:
+    header_map[cybind_module.header] = cybind_module.module_name
     return
