4230d573b489008540a9c1aa27a0a1d5cf6f0644
Gabe Ingram <gabriel.ingram@cognitivespace.com>
2021-11-07 20:54:29 -0700

    Update cybind sections with 'make_context' method that takes an entire ccmodel translation unit as input

--- src/cfactory/assemblers/cybind/cybind_sections.py
+++ src/cfactory/assemblers/cybind/cybind_sections.py
@@ -1,8 +1,10 @@
 import os
+import time
 
 from . import paths
 from cfactory.utils.wheezy_section import WheezySection
 import cfactory.utils.file_writer as fw
+import cfactory.assemblers.cybind.cybind as cybind
 
 
 template_path = os.path.join(
@@ -17,10 +19,22 @@ class HeaderSection(fw.FileSection, WheezySection):
         fw.FileSection.__init__(self, "cybind_header")
         WheezySection.__init__(self, "cybind_header.template")
         self.search_path = [template_path]
+        self.author = "Anonymous"
+        self.header = ""
+        self.ctx = {}
+        return
+
+    def make_context(
+            self,
+            t_unit: "ccmodel.code_models.variants.TranslationUnitDecl"
+            ) -> dict:
+        self.ctx["mtime"] = time.localtime()
+        self.ctx["author"] = self.author
+        self.ctx["binds"] = self.header
         return
 
     def resolve_section_text(self, **extras: dict) -> None:
-        self.section_text = self.template_eval(**extras)
+        self.section_text = self.template_eval(self.ctx)
         return
 
 
@@ -29,10 +43,21 @@ class ImportSection(fw.FileSection, WheezySection):
     def __init__(self):
         fw.FileSection.__init__(self, "import")
         WheezySection.__init__(self, "import_section.template")
+        self.cimports = []
+        self.pyimports = []
+        self.ctx = {}
+        return
+
+    def make_context(
+            self,
+            t_unit: "ccmodel.code_models.variants.TranslationUnitDecl"
+            ) -> dict:
+        self.ctx["cimports"] = self.cimports
+        self.ctx["pyimports"] = self.pyimports
         return
 
     def resolve_section_text(self, **extras: dict) -> None:
-        self.section_text = self.template_eval(**extras)
+        self.section_text = self.template_eval(self.ctx)
         return
 
 
@@ -41,10 +66,21 @@ class PxdBodySection(fw.FileSection, WheezySection):
     def __init__(self):
         fw.FileSection.__init__(self, "pxd_body")
         WheezySection.__init__(self, "pxd_body.template")
+        self.header = ""
+        self.namespace = None
+        self.ctx = {}
+        return
+
+    def make_context(
+            self,
+            t_unit: "ccmodel.code_models.variants.TranslationUnitDecl"
+            ) -> dict:
+        self.ctx["header"] = self.header
+        self.ctx["namespace"] = self.namespace
         return
 
     def resolve_section_text(self, **extras: dict) -> None:
-        self.section_text = self.template_eval(**extras)
+        self.section_text = self.template_eval(self.ctx)
         return
 
 
@@ -53,10 +89,21 @@ class PyxBodySection(fw.FileSection, WheezySection):
     def __init__(self):
         fw.FileSection.__init__(self, "pyx_body")
         WheezySection.__init__(self, "pyx_body.template")
+        self.header = ""
+        self.namespace = None
+        self.ctx = {}
+        return
+
+    def make_context(
+            self,
+            t_unit: "ccmodel.code_models.variants.TranslationUnitDecl"
+            ) -> dict:
+        self.ctx["header"] = self.header
+        self.ctx["namespace"] = self.namespace
         return
 
     def resolve_section_text(self, **extras: dict) -> None:
-        self.section_text = self.template_eval(**extras)
+        self.section_text = self.template_eval(self.ctx)
         return
 
 
