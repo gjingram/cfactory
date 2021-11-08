8bfa8aa404ca5cc3c1ea8e3fef5b7284fe92b6e9
Gabe Ingram <gabriel.ingram@cognitivespace.com>
2021-11-07 20:56:31 -0700

    Add author field for testing

--- test/cfscript.py
+++ test/cfscript.py
@@ -6,5 +6,5 @@ factory.project_name = "cfactory_test"
 factory.cache_dir = "cfactory_test_cache"
 factory.ccm_state_dir = os.path.join(factory.cache_dir, "ccm_test")
 
-cybind = CybindAssembler("test")
+cybind = CybindAssembler("test", author="Gabriel Ingram")
 cybind.add_files("test_hh/*.hh")
